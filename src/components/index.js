994526b3db45adacedb67193e1dc1af4af126cc1
RYNBSD <112697887+RYNBSD@users.noreply.github.com>
2022-09-15 14:29:24 +0100

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import NavBar from "./NavBar";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CTA from "./CTA";
+import Clients from "./Clients";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Billing from "./Billing";
+import Stats from "./Stats";
+
+export {
+    NavBar,
+    Testimonials,
+    Hero,
+    Footer,
+    CTA,
+    CardDeal,
+    Clients,
+    Billing,
+    Business,
+    Stats
+}
